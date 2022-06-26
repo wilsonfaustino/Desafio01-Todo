cc80ea0ef8d6ace57fca5a6402dae8289db911f5
Wilson Faustino <wilson.rfaustino@gmail.com>
2022-06-26 16:40:35 -0300

    changed alt to default language

--- src/components/EmptyLIst.tsx
+++ src/components/EmptyLIst.tsx
@@ -6,7 +6,7 @@ import styles from './EmptyList.module.css'
 export function EmptyList() {
     return (
         <div className={styles.empty}>
-            <img src={Clipboard} alt="Clipboard Image" />
+            <img src={Clipboard} alt="ilustração de uma lista de tarefas" />
             <strong>Você ainda não tem tarefas cadastradas</strong>
             <span>Crie tarefas e organize seus itens a fazer</span>
         </div>
